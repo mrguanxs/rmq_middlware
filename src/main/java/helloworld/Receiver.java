436cadd20509c0e57b327fec6adf9321cd375bf7
GuanXinshuaii <mrguanxs@163.com>
2019-05-15 19:58:32 +0800

    helloworld description

--- src/main/java/helloworld/Receiver.java
+++ src/main/java/helloworld/Receiver.java
@@ -12,6 +12,7 @@ public class Receiver {
     private final static String QUEUE_NAME = "hello2";
 
     public static void main(String[] argv) throws Exception {
+        //打开链接和通道
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUsername("guest");
         factory.setPassword("guest");
@@ -27,9 +28,11 @@ public class Receiver {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
+        //声明一个存在的队列用来取消息
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println("Waiting for messages. ");
+        System.out.println("Waiting for messages. 退出按CTRL + C");
 
+        //以对象形式提供回调，队列会异步的传递消息过来
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
@@ -38,7 +41,14 @@ public class Receiver {
                 System.out.println(" [x] Received '" + message + "'");
             }
         };
+//        DeliverCallback deliverCallback =(consumerTag, delivery) -> {
+//                String message = new String(delivery.getBody(), "UTF-8");
+//        System.out.println("[x] Received'" + message + "'");
+//        };
+//        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag  -> {});
         channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        //不用关闭资源保持链接和通道alive随时接收消息
     }
 
 }
--- src/main/java/helloworld/Sender.java
+++ src/main/java/helloworld/Sender.java
@@ -12,9 +12,11 @@ import java.util.concurrent.TimeoutException;
  */
 public class Sender {
 
+    //队列命名
     private final static String QUEUE_NAME = "hello2";
 
     public static void main(String[] argv)throws java.io.IOException, TimeoutException {
+        //创建到服务器的链接
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUsername("guest");
         factory.setPassword("guest");
@@ -22,9 +24,12 @@ public class Sender {
         factory.setPort(5672);
         factory.setVirtualHost("/");
         Connection connection = factory.newConnection();
+        //创建通道
         Channel channel = connection.createChannel();
+        //声明队列
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         String message = "Hello World2!";
+        //发布消息到队列
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
         System.out.println(" [x] Sent '" + message + "'");
         channel.close();
